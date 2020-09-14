d6583fc931dfc5b964f4c9569e21587241d9472f
Sy Rashid <sy.rashid.11@gmail.com>
2020-09-14 12:36:17 +0300

    FEAT: Delete function for api

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::RestaurantsController < Api::V1::BaseController
-  before_action :set_restaurant, except: [:index]
+  acts_as_token_authentication_handler_for User, except: [ :index, :show ]
+  before_action :set_restaurant, except: [:index, :create]
 
   def index
     @restaurants = policy_scope(Restaurant)
@@ -8,10 +9,46 @@ class Api::V1::RestaurantsController < Api::V1::BaseController
   def show
   end
 
+  def update
+    if @restaurant.update(restaurant_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.user = current_user
+    authorize @restaurant
+
+    if @restaurant.save
+      render :show, status: :created
+    else
+      render_error
+    end
+  end
+
+  def destroy
+    name = @restaurant.name
+    @restaurant.destroy
+    render json: { success: "#{name} was deleted"},
+    status: :ok
+  end
+
   private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address)
+  end
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
     authorize @restaurant  # For Pundit
+  end
+
+  def render_error
+    render json: { errors: @restaurant.errors.full_messages },
+      status: :unprocessable_entity
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :restaurants, only: [ :index, :show ]
+      resources :restaurants, only: [ :index, :show, :update, :create, :destroy ]
     end
   end
 end
