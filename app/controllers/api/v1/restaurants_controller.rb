c9835f457c6d65e199b4177e08278a6975227be5
Sy Rashid <sy.rashid.11@gmail.com>
2020-09-15 10:07:46 +0300

    FIX: Added comments for api controller

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,14 +1,27 @@
+# Note the namespacing
 class Api::V1::RestaurantsController < Api::V1::BaseController
+  # Makes sure the controller authenticates through the token for certain actions
   acts_as_token_authentication_handler_for User, except: [ :index, :show ]
+
   before_action :set_restaurant, except: [:index, :create]
 
   def index
     @restaurants = policy_scope(Restaurant)
   end
 
-  def show
-  end
+  def show; end
 
+  # Both the update and create are extremely similar to what we've seen before
+  # The only difference is the status associated with the return
+  # Remember these are the different HTTP requests
+
+  # HTTP Requests you Could know
+    # 2XX OK
+    # 3XX Redirect
+    # 4XX You fucked up
+    # 5XX We fucked up (server) (THING YOU SHOULD FEAR THE MOST)
+
+  # The other major different is the render_error, instead of rendering the same html page again as we did in a regular controller, for our api controller we render json that convey our errors
   def update
     if @restaurant.update(restaurant_params)
       render :show
