e02c4c6010ed755ec0c68bd7494ce4692728ffff
Sy Rashid <sy.rashid.11@gmail.com>
2020-09-15 10:04:10 +0300

    FIX: Added comments for policy

--- app/policies/restaurant_policy.rb
+++ app/policies/restaurant_policy.rb
@@ -1,4 +1,5 @@
 class RestaurantPolicy < ApplicationPolicy
+  # Note all the different policy actions we have here, it is important to ask ourselves exactly who we should give access to each one of our endpoints and protect accordingly
   class Scope < Scope
     def resolve
       scope.all
@@ -10,14 +11,17 @@ class RestaurantPolicy < ApplicationPolicy
   end
 
   def update?
+    # Does the restaurant creator match the token of the user submitting the api call
     record.user == user
   end
 
   def create?
+    # Does the user exist in the db?
     !user.nil?
   end
 
   def destroy?
+    # If I can update it, I should be able to destroy it
     update?
   end
 end
