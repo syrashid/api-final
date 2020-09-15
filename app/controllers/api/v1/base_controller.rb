56f2d24fc97cb3d581894044ded94b4ff508778f
Sy Rashid <sy.rashid.11@gmail.com>
2020-09-15 09:25:15 +0300

    FIX: Comments for application controller

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,14 +1,18 @@
 class Api::V1::BaseController < ActionController::API
+  # Remember we need Pundit here to protect our API Endpoints
   include Pundit
 
+  # Standard protections after actions for pundit
   after_action :verify_authorized, except: :index
   after_action :verify_policy_scoped, only: :index
 
+  # With APIs we need to return json when we encounter errors instead of html, these are here to do that
   rescue_from Pundit::NotAuthorizedError,   with: :user_not_authorized
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   private
 
+  # Helper methods to help us deal with Pundit and Active Record Exceptions
   def user_not_authorized(exception)
     render json: {
       error: "Unauthorized #{exception.policy.class.to_s.underscore.camelize}.#{exception.query}"
