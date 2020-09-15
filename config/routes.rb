9917c10560d351147ea24503e882c368414ec0b1
Sy Rashid <sy.rashid.11@gmail.com>
2020-09-15 09:28:55 +0300

    FIX: Added comments to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rub
+  # Our routing needs to follow the same convention as our controllers
+  # Notice what the default format is here, what happens when you run rails routes?
+
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :restaurants, only: [ :index, :show, :update, :create, :destroy ]
